a118363d854e119d31096a4260da7385f80f5715
Lige <lige.williamson@gmail.com>
2019-03-01 20:24:15 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  sorted = arr.sort_by do |phrase|
+    indicies = []
+    phrase.each_char{|index| indicies << esperanto_alphabet.index(phrase[index])}
+    indicies
+  end
 end
